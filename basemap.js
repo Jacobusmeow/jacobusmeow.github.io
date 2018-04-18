076a2ee8c410b96929c26356a51455e5a0222e2e
Jacob Heuser <jacob.heuser@student.uibk.ac.at>
2018-04-18 21:00:10 +0200

    Datenquelle Link korrigiert

--- basemap.js
+++ basemap.js
@@ -8,31 +8,31 @@ let myLayers = {
     geolandbasemap : L.tileLayer (
         "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
             subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], // die subdomains, die beim ansprechen für {s} genutzt werden
-            attribution : "Datenquelle: <a href='www,basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
         }
     ),
     bmapoverlay : L.tileLayer (
         "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
             subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], // die subdomains, die beim ansprechen für {s} genutzt werden
-            attribution : "Datenquelle: <a href='www,basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
         }
     ),
     bmapgrau : L.tileLayer (
         "https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
             subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], // die subdomains, die beim ansprechen für {s} genutzt werden
-            attribution : "Datenquelle: <a href='www,basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
         }
     ),
     bmaphidpi : L.tileLayer (
         "https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
             subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], // die subdomains, die beim ansprechen für {s} genutzt werden
-            attribution : "Datenquelle: <a href='www,basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
         }
     ),
     bmaporthofoto30cm : L.tileLayer (
         "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
             subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], // die subdomains, die beim ansprechen für {s} genutzt werden
-            attribution : "Datenquelle: <a href='www,basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" //korrekte datenquelle hinzugefügt
         }
     )
 };
