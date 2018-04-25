26fc0d8135e333017f337bfb1c48bbdc70d5254f
Heuser Jacob <jacob.heuser@student.uibk.ac.at>
2018-04-25 18:20:40 +0200

    marker patsch, igls erstellt und popup mit bild hinzugefügt

--- marker.js
+++ marker.js
@@ -1,4 +1,6 @@
 let myMap = L.map("mapdiv");
+let markerGroup = L.featureGroup();//mehrere marker in einr gruppe zusammenfügen
+
 //L.map spricht die leaflet bibliothek an und sagt wo im html die karte hin soll. mapdiv ist der link zu der stelle wo die karte hin soll
 //DOClink: http://leafletjs.com/reference-1.3.0.html#map-l-map
 let myLayers = {
@@ -7,7 +9,7 @@ let myLayers = {
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
        // subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], Osm wird nicht angezeigt, warum?
         attribution : "Datenquelle: <a href='https://www.openstreetmap.org'>osm.org"
-    } 
+        } 
     ),
     geolandbasemap : L.tileLayer (
         "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
@@ -44,7 +46,7 @@ let myLayers = {
 
 myMap.addLayer(myLayers.osm);//DOCLink: http://leafletjs.com/reference-1.3.0.html#map-addlayer
 //fügt die verschieden Layer hinzu 
-
+myMap.addLayer(markerGroup);
 
 let myMapControl = L.control.layers({ //DOCLink: http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
     "Openstreetmap" : myLayers.osm,
@@ -54,6 +56,7 @@ let myMapControl = L.control.layers({ //DOCLink: http://leafletjs.com/reference-
     "Orthofoto 30cm" : myLayers.bmaporthofoto30cm,
 },{
     "bmapoverlay" : myLayers.bmapoverlay, // schrift kann in allen anderen Layern angezeigt werden
+    "Marker" : markerGroup, //die marker können in der layer control an und ausgeschaltet werden. 
 },{
     collapsed : false, //DOCLink: http://leafletjs.com/reference-1.3.0.html#control-layers-collapsed
     //ich bin mir nicht sicher, ob ich die aufgabe richtig verstanden habe, aber jetzt ist das layer control direkt ausgeklappt..
@@ -85,17 +88,30 @@ const uni = [47.264, 11.385];
 // konstante variable hinzugefügt, um die koordinaten nicht ständig eingeben zu müssen, Konstante für die Optionen der unterschiedlichen Marker angegeben
 const usi = [47.257, 11.356];
 const technik = [47.263, 11.343];
+const patscherkofl = [47.218589, 11.466667];
+const igls = [47.232782, 11.408325];
 const markerOptions = {
     title : "Universität Innsbruck", 
     opacity : 0.8, 
     draggable : true,
 };
-L.marker(uni, markerOptions).addTo(myMap);
+L.marker(uni, markerOptions).addTo(markerGroup);
 //marker mit der Position der Hauptuni Innsbruck hinzugefügt
 
-L.marker(usi, markerOptions).addTo(myMap);
+L.marker(usi, markerOptions).addTo(markerGroup);
 
-L.marker(technik, markerOptions).addTo(myMap);
+L.marker(technik, markerOptions).addTo(markerGroup);
+
+L.marker(patscherkofl).addTo(markerGroup);
+L.marker(igls).addTo(markerGroup);
+myMap.fitBounds(markerGroup.getBounds()); //wie sind die marker verteilt? der anzeigte ausschnitt wird so gewählt, dass alle zu sehen sind. 
+
+let patscherkoflMarker = L.marker(patscherkofl).addTo(markerGroup);
+patscherkoflMarker.bindPopup("<p>Igls vom Patscherkofl</p><img style='width:200px' src='js/leaflet/images/iglsvonpatsch.jpg' alt='Patscherkofl Aussicht auf Igls' />");
 
 
-myMap.setView(uni, 16);
+
+//marker.bindPopup().openPopup().addTo(myMap);
+
+
+//src="js\leaflet\images\iglsvonpatsch.jpg"
