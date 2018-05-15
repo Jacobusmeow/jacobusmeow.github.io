714839a350a5a44d42e45d6110afe1c75acd88ef
Jacob Heuser <jacob.heuser@student.uibk.ac.at>
2018-05-15 12:05:30 +0200

    Workload 8 final

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -29,92 +29,92 @@
 
 let meineKarte = L.map("map");
 let markerGruppe = L.featureGroup();
+let koordGruppe = L.featureGroup();
 let kartenLayer={
     osm: L.tileLayer(
-        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { //warum geht nur das erste Zoomlevel?
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { 
             attribution : "Datenquelle: <a href='https://www.openstreetmap.org'>osm.org"
     }),
     geolandbasemap : L.tileLayer (
         "https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
             subdomains : ["maps", "maps1", "maps2", "maps3"], 
             attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" 
-    }), //ortho, winter und sommer werden nicht korrekt geladen! 
+    }),
+    bmapoverlay : L.tileLayer (
+        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains : ["maps", "maps1", "maps2", "maps3", "maps4"], 
+            attribution : "Datenquelle: <a href='https://www.basemap.at'>basemap.at" 
+        }),
     ortho : L.tileLayer(
-        "http://wmts.kartetirol.at/wmts/gdi_ortho/GoogleMapsCompatible/{z}/{y}/{x}.jpeg80", {
-            //subdomains : ["a", "b", "c"],
+        "http://wmts.kartetirol.at/wmts/gdi_ortho/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+           
             attribution : "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at"
     }),
     winter : L.tileLayer(
-        "http://wmts.kartetirol.at/wmts/gdi_winter/GoogleMapsCompatible/{z}/{y}/{x}.jpeg80", {
-            //subdomains : ["a", "b", "c"],
+        "http://wmts.kartetirol.at/wmts/gdi_winter/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+           
             attribution : "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at"
     }),
     sommer : L.tileLayer(
-        "http://wmts.kartetirol.at/wmts/gdi_summer/GoogleMapsCompatible/{z}/{y}/{x}.jpeg80", {
-            //subdomains : ["a", "b", "c"],
+        "http://wmts.kartetirol.at/wmts/gdi_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80", {
+          
             attribution : "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>data.gv.at"
     }),    
 
 }
 
 meineKarte.addLayer(kartenLayer.osm);
-
-
-
-
 //meineKarte.setView([47.449448, 12.392538], 11);
-
 let maßstab = L.control.scale({
     imperial : false, 
     metric : true, 
     position : "bottomleft",
     maxWidth: 200
 }).addTo(meineKarte); 
+//Controlfenster: 
+let meineKarteControl = L.control.layers({
+    "Openstreetmap" : kartenLayer.osm, 
+    "Geoland Basemap" : kartenLayer.geolandbasemap,
+    "Orthofoto" : kartenLayer.ortho,
+    "Winter" : kartenLayer.winter,
+    "Sommer" : kartenLayer.sommer
+},{
+    //overlays
+    "Beschriftung" : kartenLayer.bmapoverlay,
+    "Etappe 11 - Koordinaten" : koordGruppe,
+    "Start & Ziel Marker" : markerGruppe,
+    //Ziel" :  markerGruppe,
+});
+//marker + popups 
+const start = [47.668714, 12.404299];
+const ziel = [47.44637, 12.390424];
+L.marker(start);
+L.marker(ziel);
+let startMarker = L.marker(start,
+    {
+    icon : L.icon({
+        iconUrl : "images/start.png",
+        iconAnchor : [16, 37]
+    })}).addTo(markerGruppe);
+    startMarker.bindPopup("<h3>Start in Kössen im Kaiserwinkel</h3><p><a href='https://de.wikipedia.org/wiki/Kössen'>Informationen</a></p>");
+let zielMarker = L.marker(ziel,
+    {
+    icon : L.icon({
+        iconUrl : "images/finish.png",
+        iconAnchor : [16, 37] 
+        
+})}).addTo(markerGruppe);
+    zielMarker.bindPopup("<h3>Ziel in Kitzbühel</h3><p><a href='https://de.wikipedia.org/wiki/Kitzbühel'>Informationen</a></p>");
 //strecke: 
-let geojson = L.geoJSON(data11).addTo(markerGruppe);
+let geojson = L.geoJSON(data11).addTo(koordGruppe);
 geojson.bindPopup(function(layer){
     console.log("Layer for Popup:", layer.feature.geometry);
     const props = layer.feature.geometry;
     const popupText =`<p>${props.coordinates}</p>`;
     return popupText; 
 });
-//Controlfenster: 
-let meineKarteControl = L.control.layers({
-    "Openstreetmap" : kartenLayer.osm, 
-    "Geoland Basemap" : kartenLayer.geolandbasemap,
-    "Orthofoto" : kartenLayer.ortho,
-    "Ortho_Winter" : kartenLayer.winter,
-    "Ortho_Sommer" : kartenLayer.sommer
-},{
-    //overlays
-    "Etappe 11 - Koordinaten" : markerGruppe,
-    //"Start" : L.marker(start),//markerGruppe,
-    //"Ziel" :  L.marker(ziel)//markerGruppe,
-})
-//marker + popups 
-const start = [47.668714, 12.404299];
-const ziel = [47.44637, 12.390424];
-L.marker(start);//.addTo(markerGruppe);
-L.marker(ziel);//.addTo(markerGruppe);
-let startMarker = L.marker(start,
-    {
-    icon : L.icon({
-        iconUrl : "images/start.png"
-    })}).addTo(markerGruppe);
-    startMarker.bindPopup("<h3>Start in Kössen im Kaiserwinkel</h3><p><a href='https://de.wikipedia.org/wiki/Kössen'>Informationen</a></p>");
-let zielMarker = L.marker(ziel,
-    {
-    icon : L.icon({
-        iconUrl : "images/finish.png"
-})}).addTo(markerGruppe);
-    zielMarker.bindPopup("<h3>Ziel in Kitzbühel</h3><p><a href='https://de.wikipedia.org/wiki/Kitzbühel'>Informationen</a></p><img src='images/kitzbuehel.jpg' alt='Bild von Kitzbuehel'/>");
-
-   
-
 //zur Karte hinzufügen: 
-
 meineKarte.addControl(meineKarteControl);
-
 //Zoom auf alle Marker und Popups
 meineKarte.fitBounds(markerGruppe.getBounds());
 
